a1821b398bc53830fdbd039a0d3464c603df4874
NukelordPvP <zmpeternell@gmail.com>
2022-02-14 00:53:05 -0800

    made max xzscale go to double max value so like 1quintillion

--- src/main/java/me/nukelord/farlandsfabric/mixins/MixinNoiseSamplingConfig.java
+++ src/main/java/me/nukelord/farlandsfabric/mixins/MixinNoiseSamplingConfig.java
@@ -11,10 +11,10 @@ import net.minecraft.world.gen.chunk.NoiseSamplingConfig;
 public class MixinNoiseSamplingConfig {
 	@Redirect(method = "<clinit>", at = @At(target = "Lcom/mojang/serialization/Codec;doubleRange(DD)Lcom/mojang/serialization/Codec;", value = "INVOKE"))
 	private static Codec<Double> fixRange(double min, double max) {
-		return Codec.doubleRange(min, 10000000);
+		return Codec.doubleRange(min, max);
 
 		// changes datapack xzScale range and other ranges
-		// to be 0.001 to 10 million instead of 1 thousand
-		// so you can move farlands within 125 of spawn.
+		// to be 0.001 to max a double can handle instead of 1 thousand
+		// so you can move farlands within 125 of spawn and EVEN generate the 64bit farlands.
 	}
 }
